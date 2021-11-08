28ee0f3eed2bc3b4f235239c84d23942a81b7438
JeniLopez <jelopez@escuelasproa.edu.ar>
2021-11-08 12:19:21 -0300

    Ahora anda (Arreglar grilla)
    espy+jere+jeni

--- server.js
+++ server.js
@@ -9,22 +9,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 let productos = [
-    { nombre: "tomate", tipo: "fruta", precio: "$50", stock: "true" },
-    { nombre: "papa", tipo: "verdura", precio: "$20", stock: "false" },
-    { nombre: "espinaca", tipo: "verdura", precio: "$15", stock: "true" },
-    { nombre: "uva", tipo: "fruta", precio: "$200", stock: "true" },
-    { nombre: "frutilla", tipo: "fruta", precio: "$150", stock: "false" },
-    { nombre: "sandia", tipo: "fruta", precio: "$100", stock: "true" },
-    { nombre: "manzana", tipo: "fruta", precio: "$85", stock: "true" },
-    { nombre: "pera", tipo: "fruta", precio: "$40", stock: "false" },
-    { nombre: "palta", tipo: "verdura", precio: "$77", stock: "true" },
+    { nombre: "tomate", tipo: "fruta", precio: "$50", stock: "true", ruta: "/img/tomate.jpg"},
+    { nombre: "papa", tipo: "verdura", precio: "$20", stock: "false", ruta: "/img/papas.jpg" },
+    { nombre: "espinaca", tipo: "verdura", precio: "$15", stock: "true" , ruta: "/img/espinaca.jpg"},
+    { nombre: "uva", tipo: "fruta", precio: "$200", stock: "true", ruta: "/img/uvas.jpg" },
+    { nombre: "lechuga", tipo: "verdura", precio: "$150", stock: "false", ruta: "/img/lechuga.jpg" },
+    { nombre: "sandia", tipo: "fruta", precio: "$100", stock: "true", ruta: "/img/sandia.jpg" },
+    { nombre: "manzana", tipo: "fruta", precio: "$85", stock: "true", ruta: "/img/manzana.jpg" },
+    { nombre: "pera", tipo: "fruta", precio: "$40", stock: "false" , ruta: "/img/pera.jpg"},
+    { nombre: "palta", tipo: "verdura", precio: "$77", stock: "true" , ruta: "/img/palta.jpg"},
 ];
 
 app.get("/verduleria", (req, res) => {
-    // res.render("index", { productos: productos });
-    res.render("index", productos);
+res.render("index", { productos: productos });
+   // res.render("index", productos);
 });
 
 app.listen(port, () => {
